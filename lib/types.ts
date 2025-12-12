97b9fdf7220d886f9398bce1f3d0a8c7d68cdb3d
Ian Holser <ifholser@ucdavis.edu>
2025-12-12 10:59:08 -0800

    add lib

---
+++ lib/types.ts
@@ -0,0 +1,28 @@
+export interface Product {
+  id: string
+  name: string
+  description: string
+  price: number
+  category: string
+  image: string
+}
+
+export interface CartItem {
+  product: Product
+  quantity: number
+}
+
+export interface Cart {
+  items: CartItem[]
+  total: number
+}
+
+export interface Order {
+  id: string
+  customerName: string
+  customerEmail: string
+  shippingAddress: string
+  items: CartItem[]
+  total: number
+  createdAt: Date
+}
